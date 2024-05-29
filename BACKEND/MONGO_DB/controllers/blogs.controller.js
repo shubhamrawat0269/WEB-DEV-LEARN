002ce01221c83f4520abc3bc90b5a362ff9d8f40
shubham Rawat <rawatshubam269@gmail.com>
2024-05-29 17:08:01 +0530

    Service Layer Added

--- BACKEND/MONGO_DB/controllers/blogs.controller.js
+++ BACKEND/MONGO_DB/controllers/blogs.controller.js
@@ -13,11 +13,10 @@ const getAllBlogs = async (req, res) => {
 
 const createNewBlog = async (req, res) => {
   try {
-    const newBlogDoc = await Blogs.create(req.body);
+    await Blogs.create(req.body);
     // console.log(newBlogDoc);
     return res.sendStatus(200);
   } catch (error) {
-    console.log(error);
     return res
       .status(400)
       .json({ message: "duplicate title is strictly prohibited" });
--- BACKEND/MONGO_DB/schema/blogs.schema.js
+++ BACKEND/MONGO_DB/schema/blogs.schema.js
@@ -1,15 +1,21 @@
 const mongoose = require("mongoose");
 
+/*  nested schema   */
+const authorSchema = new mongoose.Schema({
+  fullName: { type: String, maxlength: 25 },
+  twitterHandle: { type: String },
+  email: { type: String, required: true, maxlength: 50 },
+  image: { type: String },
+});
+
 const blogSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, unique: true },
-    authors: { type: [String] },
+    title: { type: String, required: true },
+    author: [authorSchema],
     content: { type: String, default: "" },
     publishedAt: { type: Date, default: null },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 module.exports = blogSchema;
