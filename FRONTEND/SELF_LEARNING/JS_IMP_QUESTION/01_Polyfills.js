3843eccc6e2f3b781b35f209e8bae73fd8e5e288
shubham Rawat <rawatshubam269@gmail.com>
2024-07-08 14:31:44 +0530

    Grid Project Partially Done

--- FRONTEND/SELF_LEARNING/JS_IMP_QUESTION/01_Polyfills.js
+++ FRONTEND/SELF_LEARNING/JS_IMP_QUESTION/01_Polyfills.js
@@ -1,4 +1,7 @@
-// write polyfill of promise.all()
+// -----------------------------------------------------------------------
+/*
+ * Write the polyfill of promise.all()
+ */
 
 const dummyApi = (time) => {
   /** customise promise for dummy api */
@@ -12,7 +15,6 @@ const dummyApi = (time) => {
 /* no. of asynchronous task in form of array */
 const tasks = [dummyApi(1000), dummyApi(2000), dummyApi(3000)];
 
-/* promise all polyfill */
 const promiseAllPolyfill = (tasks) => {
   return new Promise((res, rej) => {
     let output = [];
@@ -35,5 +37,27 @@ promiseAllPolyfill(tasks)
   })
   .catch((er) => console.log(er));
 
+// -----------------------------------------------------------------------
 
+/*
+ * Write the polyfill of map()
+ */
 
+const arr = [1, 2, 3, 4, 5];
+
+// arr.map((curr, index) => console.log(curr * 2));
+
+Array.prototype.myMap = function (cb) {
+  let arr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    arr.push(cb(this[i], i, this));
+  }
+
+  return arr;
+};
+
+const newArr = arr.myMap((curr, index, a) => curr * 2);
+// console.log(newArr);
+
+// -----------------------------------------------------------------------
