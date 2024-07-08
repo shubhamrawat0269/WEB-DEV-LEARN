2e34c1d604c5fc524ccf4e36a2bcf7e384ef4a9e
shubham Rawat <rawatshubam269@gmail.com>
2024-07-08 19:49:23 +0530

    Nullyfying Mini Projects component folder

--- FRONTEND/SELF_LEARNING/REACT_MINI_PROJECTS/src/Routes.jsx
+++ FRONTEND/SELF_LEARNING/REACT_MINI_PROJECTS/src/Routes.jsx
@@ -1,69 +1,7 @@
 import React, { useContext, useEffect } from "react";
-import {
-  Navbar,
-  JobBoardBulleting,
-  Quiz,
-  PwdGenerator,
-  EmployeeDB,
-  CurrencyConvertor,
-  DraggableNodes,
-  TodoList,
-  FAQ,
-  ProgressBar,
-  CountDownTimer,
-  InfiniteScroll,
-  MortgageCalculator,
-  OtpLogin,
-  MultiInputSearch,
-  CustomCarousel,
-  FormWithoutYup,
-  NavbarTailwind,
-  Slider,
-  TwoFactorCode,
-  TinderSwiper,
-} from "./components/index";
-// import { GlobalContext } from "./contexts/GlobalContext";
+
 const Routes = () => {
-  // const { theme } = useContext(GlobalContext);
-
-  // useEffect(() => {
-  //   let mainDiv = document.getElementById("primary__section");
-  //   if (theme === "") {
-  //     mainDiv.classList.remove("dark");
-  //   } else {
-  //     mainDiv.classList.add("dark");
-  //   }
-  // }, [theme]);
-
-  return (
-    <div id="primary__section" className="h-screen">
-      {/* <EmployeeDB /> */}
-      {/* <Navbar /> */}
-      {/* <NavbarTailwind /> */}
-
-      {/* <CustomCarousel /> */}
-      {/* <Slider /> */}
-
-      {/* <InfiniteScroll /> */}
-
-      {/* <MultiInputSearch /> */}
-
-      {/* <OtpLogin /> */}
-      {/* <TwoFactorCode /> */}
-      {/* <CountDownTimer /> */}
-      {/* <MortgageCalculator /> */}
-      {/* <FAQ /> */}
-      {/* <Quiz /> */}
-      {/* <PwdGenerator /> */}
-      {/* <CurrencyConvertor /> */}
-      {/* <JobBoardBulleting /> */}
-      {/* <DraggableNodes /> */}
-      {/* <TodoList /> */}
-      {/* <ProgressBar /> */}
-      {/* <FormWithoutYup /> */}
-      <TinderSwiper />
-    </div>
-  );
+  return <div id="primary__section" className="h-screen"></div>;
 };
 
 export default Routes;
--- FRONTEND/SELF_LEARNING/REACT_MINI_PROJECTS/src/components/TinderSwiper/TinderSwiper.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TinderSwiper = () => {
-  return <div>TinderSwiper</div>;
-};
-
-export default TinderSwiper;
