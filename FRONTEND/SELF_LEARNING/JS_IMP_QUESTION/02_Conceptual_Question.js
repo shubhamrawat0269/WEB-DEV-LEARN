828b486029ff7c6beb4cf7b5ad1cb7a3590c2c59
shubham Rawat <rawatshubam269@gmail.com>
2024-07-26 11:45:42 +0530

    js conceptual questions learning

---
+++ FRONTEND/SELF_LEARNING/JS_IMP_QUESTION/02_Conceptual_Question.js
@@ -0,0 +1,22 @@
+/* ------------------------------------------------  */
+
+let num = 20;
+console.log(num + "20");
+console.log(++num);
+
+/* ------------------------------------------------  */
+
+console.log(parseInt("108"));
+console.log(parseInt("108x102"));
+console.log(parseInt("108Machines"));
+
+/* ------------------------------------------------  */
+
+const name = "Shubham";
+age = 19;
+console.log(delete name);
+console.log(delete age);
+
+/* Note : Undeclared variable associted with window object so ln19. will delete age as it is inside an object and return true */
+
+/* ------------------------------------------------  */
