a1dcc17d03ceead7f3b4bfab9b4e393df26f87ec
shubham Rawat <rawatshubam269@gmail.com>
2024-08-06 09:10:42 +0530

    vue learning

--- FRONTEND/VUE-CRASH-2024/VUE-CLI/src/App.vue
+++ FRONTEND/VUE-CRASH-2024/VUE-CLI/src/App.vue
@@ -1,4 +1,19 @@
+<script>
+ export default {
+  data(){
+    return {
+      username: "Shubham Rawat",
+      login: false,
+      tasks: ["Learn Basics of Vue", "Task two", "Task three", "Task four",]
+    }
+  }
+ }
+</script>
+
 <template>
-  Hello Shubham
+  <h1>Hello {{username}}</h1>
+  <p v-if="login">User is Active</p>
+  <p v-else>User is Not Active</p>
 </template>
+
 
