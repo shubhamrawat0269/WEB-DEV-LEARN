c95d4145bd087aaacea6043afa033da3a04f60b3
shubham Rawat <rawatshubam269@gmail.com>
2024-08-10 12:49:01 +0530

    vue app learning start day 1

--- FRONTEND/VUE-CRASH-2024/VUE-CLI/src/App.vue
+++ FRONTEND/VUE-CRASH-2024/VUE-CLI/src/App.vue
@@ -1,19 +1,10 @@
-<script>
- export default {
-  data(){
-    return {
-      username: "Shubham Rawat",
-      login: false,
-      tasks: ["Learn Basics of Vue", "Task two", "Task three", "Task four",]
-    }
-  }
- }
+<script setup>
+  
 </script>
 
 <template>
-  <h1>Hello {{username}}</h1>
-  <p v-if="login">User is Active</p>
-  <p v-else>User is Not Active</p>
+ <h1> This is header comp</h1>
+  <RouterView />
 </template>
 
 
--- FRONTEND/VUE-CRASH-2024/VUE-CLI/src/main.js
+++ FRONTEND/VUE-CRASH-2024/VUE-CLI/src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+
+const routes = [
+{ path: '/about', component: About },
+  { path: '/', component: Home },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
