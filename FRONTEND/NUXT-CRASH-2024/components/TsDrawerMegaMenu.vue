2a4f1c9610d17e0d0a1d586970075abe5a949ad0
shubham Rawat <rawatshubam269@gmail.com>
2024-08-15 13:21:08 +0530

    Drawer App Mobile Version Created

---
+++ FRONTEND/NUXT-CRASH-2024/components/TsDrawerMegaMenu.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="fixed inset-0 bg-slate-700 bg-opacity-40 z-10">
+        <div class="bg-white w-4/5 h-full absolute right-0 rounded-tl-lg rounded-bl-lg">
+
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
--- FRONTEND/NUXT-CRASH-2024/pages/index.vue
+++ FRONTEND/NUXT-CRASH-2024/pages/index.vue
@@ -1,12 +1,26 @@
 <script setup>
-/*  learn to use VueUse */
-
+import { ref } from "vue";
 const { handleOnHomeClick } = useUtils();
 handleOnHomeClick()
+
+const hover = ref(false);
+
 </script>
 
 <template>
-    <div class="grid grid-cols-3 gap-5 max-w-7xl m-auto pt-5">
+    <!-- <div class="grid grid-cols-3 gap-5 max-w-7xl m-auto pt-5">
         <BlogCard />
-    </div>
+    </div> -->
+
+    <!-- <button class="bg-teal-500 text-white p-2" @mouseover="hover = true" @mouseleave="hover = false">On Hover</button>
+    <div class="bg-neutral-900">
+        <div v-if="hover" class="p-4 max-w-7xl mx-auto my-2 bg-white"> Lorem ipsum dolor sit amet consectetur
+            adipisicing
+            elit.
+            Facere ipsam, odio fuga vitae dignissimos, deleniti officiis, minus magnam veritatis totam nemo nihil id?
+            Obcaecati nobis aliquid quos id placeat incidunt amet, deserunt veritatis assumenda illum recusandae, eaque
+            aspernatur esse. Labore magni libero neque reprehenderit quae impedit iste mollitia eius dolor? </div>
+    </div> -->
+
+    <TsDrawerMegaMenu />
 </template>
