f79ee8af7cba41acc247ead06f79d715e9a32459
shubham Rawat <rawatshubam269@gmail.com>
2024-08-27 08:58:38 +0530

    Toggle Comp Created

---
+++ FRONTEND/NUXT-CRASH-2024/components/TsToggleComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="tag-toggle">
+        <div class="tags">
+            <button v-for="(tag, index) in tags" :key="index" @click="currentTag = tag"
+                :class="{ active: currentTag === tag }">
+                {{ tag }}
+            </button>
+        </div>
+        <div class="content">
+            <p v-if="currentTag === 'About DeWALT'">Content for Tag 1</p>
+            <p v-if="currentTag === 'Products'">Content for Tag 2</p>
+            <p v-if="currentTag === 'Deals'">Content for Tag 3</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tags = ref(['About DeWALT', 'Products', 'Deals']);
+const currentTag = ref(tags.value[0]);
+</script>
+
+<style scoped>
+.tag-toggle {
+    padding: 20px;
+    border: 1px solid #ccc;
+    width: 300px;
+}
+
+.tags {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 20px;
+}
+
+button {
+    padding: 5px 10px;
+    border: none;
+    background-color: #eee;
+    cursor: pointer;
+}
+
+button.active {
+    background-color: #007bff;
+    color: #fff;
+}
+
+.content p {
+    margin: 0;
+}
+</style>
--- FRONTEND/NUXT-CRASH-2024/pages/index.vue
+++ FRONTEND/NUXT-CRASH-2024/pages/index.vue
@@ -33,18 +33,19 @@ const toolImages = ["images/tool-4.png"];
     <!-- <TsImageWithFallBack fallbackText="Chess" /> -->
     <!-- <TsDrawerMegaMenu /> -->
 
-    <div class="flex gap-2">
+    <!-- <div class="flex gap-2">
         <TsImageWithlabel imageSrc="images/tool-1.jpg" imageWidth="95px" imageHeight="100px" imageAlt="MyLogo"
             cardWidth="115px" cardHeight="108px" text="Accessories" />
 
         <TsImageWithlabel imageSrc="images/tool-2.jpg" imageWidth="95px" imageHeight="100px" imageAlt="MyLogo"
             cardWidth="115px" cardHeight="108px" text="Accessories" />
+    </div> -->
 
-    </div>
-
-    <div class="my-8 max-w-screen-xl mx-auto">
+    <!-- <div class="my-8 max-w-screen-xl mx-auto">
         <TsBannerWithCta backgroundColor="#F9E400" brandLogo="images/logo-1.png" heading="Lorem" text="ipsum"
             bannerPadding="3rem 20px" buttonText="Shop More" discountText="Get 50% Off"
             discountTag="images/discount.png" :images="toolImages" />
-    </div>
+    </div> -->
+
+    <TsToggleComponent />
 </template>
