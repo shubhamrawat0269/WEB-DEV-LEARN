95c4f03fa073bb7ad1261e09c855f21ac2f8c7a4
Shubham Rawat <rawatshubam269@gmail.com>
2024-11-24 18:38:29 +0530

    Added : Hired Application Created

---
+++ FRONTEND/PROJECT/hired/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="font-bold">App Content</h1>;
+}
+
+export default App;
--- FRONTEND/PROJECT/teams-employee-app/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="font-normal">App Content</h1>;
-}
-
-export default App;
