054e16197e1efadd4f37842b49c5a6eaa35695cc
Shubham Rawat <rawatshubam269@gmail.com>
2025-07-06 22:15:33 +0530

    Day 1 : Introduction to Javascript

---
+++ FRONTEND/03.Javascript/index.js
@@ -0,0 +1,22 @@
+/*
+
+ ------------- Day 1 of Javascript daily dose -------------
+ 1. Javascript is high level, interpreted and compiled language in which code written in simple language, 
+ goes to compiler & interpretor which convert our english code to binary code which computer comprehend.
+
+ 2. After v8 engine, Javascript has interpretor as well as compiler.
+ 3. Steps behind Network request of html, css & javascript.
+  3a. Live server created a basic, fundamental level server which respond on browser to fetch --> 
+    i. HTML
+    ii. CSS
+    iii. Javascript File
+
+ 4. Feature of Preserve Log inside network tab.
+ 5. REPL (console) in browser.
+ 6. In Javascript, BODMAS rule not apply whichever (/ or *) (+ or -) comes first will get execute from L 2 R.
+   Example --> 8-5+6, 
+   Note : Brackets play a vital role in mathematical calculation and it takes priority.
+
+*/
+
+console.log("Hello Shubham Rawat ~ How are you");
