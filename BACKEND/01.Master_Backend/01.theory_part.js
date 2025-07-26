28dda421adb19f58b33146f820410d668765d9be
Shubham Rawat <rawatshubam269@gmail.com>
2025-07-27 00:11:04 +0530

    updated : day 1 of matery backend

---
+++ BACKEND/01.Master_Backend/01.theory_part.js
@@ -0,0 +1,41 @@
+/*
+
+ ------------- Day 1 of Mastery Backend -------------
+ 1. The backend refers to the server-side part of a software application. 
+    It handles data processing, business logic, database operations, and server configuration.
+ 2. Components of Backend - 
+    i. Server:
+     - A computer or cloud instance where your application runs.
+
+    ii. Application (Logic) -
+      Written in backend programming languages (like Node.js, Python, Java, PHP). 
+      It processes requests, applies business rules, and prepares data.
+
+    iii. Database:
+    Stores and retrieves data (like user info, product listings, posts). Examples:
+    ~ SQL: MySQL, PostgreSQL
+    ~ NoSQL: MongoDB, Firebase
+
+    iv. API (Application Programming Interface):
+    Acts as a messenger between the frontend and backend. 
+    It receives requests and sends responses (often in JSON format).
+ 3. Both CLI (Command-Line Interface) and GUI (Graphical User Interface) are ways 
+    for users to interact with a computer or software.
+    ~ 1. CLI (Command-Line Interface)
+    -> Definition: A text-based interface where users type commands to perform tasks.
+    -> Used by: Developers, system admins, power users.
+    ~ 2. GUI (Graphical User Interface)
+    -> Definition: A visual interface with buttons, icons, windows, and menus.
+    -> Used by: General users, designers, etc.
+
+    - yt-dlp is an application of CLI. Use it practically.
+
+*/
+
+// ---------------------------------------------------------------------------- //
+
+/*
+
+ ------------- Day 2 of Mastery Backend -------------
+
+*/
