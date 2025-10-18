f3d73519cd0d95b7eec0d0f9417c7104efda825e
Shubham Rawat <rawatshubam269@gmail.com>
2025-10-18 15:40:23 +0530

    Added : Variable using var, let & const

---
+++ FRONTEND/03.javascript/02.variable-in-js/index.js
@@ -0,0 +1,11 @@
+let firstname = "Shubham";
+let lastname = "Rawat";
+let age = 25;
+
+let isHappy = false;
+
+const intro = `My name is ${firstname} ${lastname}. I am ${age} year old ${
+  isHappy ? "and i am happy." : "and i am bliss."
+}`;
+
+
