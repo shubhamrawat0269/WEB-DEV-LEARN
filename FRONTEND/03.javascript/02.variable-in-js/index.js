ab3fb7a7c524e8923de37c8202a6e366f4f384c6
Shubham Rawat <rawatshubam269@gmail.com>
2025-10-19 18:57:56 +0530

    Added : variable in js learning

--- FRONTEND/03.javascript/02.variable-in-js/index.js
+++ FRONTEND/03.javascript/02.variable-in-js/index.js
@@ -1,11 +1,14 @@
+// debugger;
 let firstname = "Shubham";
 let lastname = "Rawat";
 let age = 25;
-
+var profession = "Web Developer";
 let isHappy = false;
 
-const intro = `My name is ${firstname} ${lastname}. I am ${age} year old ${
-  isHappy ? "and i am happy." : "and i am bliss."
-}`;
+console.log(printIntro());
 
-
+function printIntro() {
+  return `My name is ${firstname} ${lastname}. I am ${age} year old ${
+    isHappy ? "and i am happy." : "and i am bliss."
+  }`;
+}
