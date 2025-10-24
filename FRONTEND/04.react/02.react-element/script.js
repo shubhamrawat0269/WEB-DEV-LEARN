0fdc6139b8fc35a2304da55ef8e14f35693d1b8e
Shubham Rawat <rawatshubam269@gmail.com>
2025-10-24 08:31:38 +0530

    updated : react element

--- FRONTEND/04.react/02.react-element/script.js
+++ FRONTEND/04.react/02.react-element/script.js
@@ -3,10 +3,21 @@ const primaryContainer = React.createElement(
   {
     className: "container",
     title: "primary-container",
+    key: 1,
   },
-  "This is the first primary container i created"
+  [
+    React.createElement("div", { key: 2 }, [
+      React.createElement("p", { key: 3 }, "React para text"),
+      React.createElement("img", {
+        key: 4,
+        src: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+        alt: 'react-image',
+        width: 220,
+        height: 220
+      }),
+    ]),
+  ]
 );
-
 
 const root = ReactDOM.createRoot(document.querySelector("#main"));
 root.render(primaryContainer);
