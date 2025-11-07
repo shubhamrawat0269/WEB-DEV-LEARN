085da26eee425dc23a17be2def4114cf8a6f84d0
Shubham Rawat <rawatshubam269@gmail.com>
2025-11-07 09:35:18 +0530

    updated : render multiple elements

---
+++ FRONTEND/04.react/05.render-mutliple-elements/script.js
@@ -0,0 +1,37 @@
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+const Card = (key, title, body) => {
+  return (
+    <div className="card" key={key}>
+      <img
+        width={200}
+        height={200}
+        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a5c4293f8%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a5c4293f8%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.1937484741211%22%20y%3D%2296.24000034332275%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+        alt="thumbnail-image"
+      />
+
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <button>click</button>
+    </div>
+  );
+};
+
+// const CardContent = [Card(1), Card(2), Card(3), Card(4), Card(5)];
+// const cardContainer = <div className="card-container">{CardContent}</div>;
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(container);
+
+// Trigger GET call
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const container = data.map((card) => Card(card.id, card.title, card.body));
+
+  const cardContainer = <div className="card-container">{container}</div>;
+  root.render(cardContainer);
+}
+
+getData();
