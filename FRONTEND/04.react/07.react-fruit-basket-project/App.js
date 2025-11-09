673d0bc487afb512d097852a7c787e4483a3cd77
Shubham Rawat <rawatshubam269@gmail.com>
2025-11-09 21:32:40 +0530

    Updated : Component wise changes implemented in project

---
+++ FRONTEND/04.react/07.react-fruit-basket-project/App.js
@@ -0,0 +1,11 @@
+import BucketContainer from "./components/BucketContainer";
+
+const App = () => {
+  return (
+    <div>
+      <BucketContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ FRONTEND/04.react/07.react-fruit-basket-project/components/BucketContainer.js
@@ -0,0 +1,17 @@
+import AppleCounter from "./AppleCounter";
+import CustomButton from "./CustomButton";
+
+const BucketContainer = () => {
+  return (
+    <div className="container">
+      <AppleCounter value={10} label={`Basket 1`} />
+      <div className="btns">
+        <CustomButton label={`Add (Basket 1)`} />
+        <CustomButton label={`Add (Basket 2)`} />
+      </div>
+      <AppleCounter value={0} label={`Basket 2`} />
+    </div>
+  );
+};
+
+export default BucketContainer;
