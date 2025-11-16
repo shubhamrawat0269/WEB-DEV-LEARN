77ee09b7a2849f76bb5db22c25a4dbb6f0c1e34d
Shubham Rawat <rawatshubam269@gmail.com>
2025-11-16 12:14:01 +0530

    Added : Header Styling Done

--- FRONTEND/04.react/09.countries-project/components/header/Header.jsx
+++ FRONTEND/04.react/09.countries-project/components/header/Header.jsx
@@ -1,7 +1,17 @@
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <div className={styles.bgColor}>Header</div>;
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <span className={styles.logo}>Where in the world?</span>
+
+        <span className={styles.modeBtn}>
+          <button>Dark Mode</button>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
