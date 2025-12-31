dd98d356e2a4db0300ee6c7f77e04ac85f13bcf0
Shubham Rawat <rawatshubam269@gmail.com>
2025-12-31 10:24:47 +0530

    updated : understanding react element

--- FRONTEND/04.react/02.react-element/script.js
+++ FRONTEND/04.react/02.react-element/script.js
@@ -1,23 +1,93 @@
-const primaryContainer = React.createElement(
-  "section",
+/* 
+ Below code is how we can create a React Element i.e. a javascript object with some properties.
+*/
+
+const paragraph = React.createElement(
+  "p",
   {
+    id: "para",
+    key: "main-container",
     className: "container",
-    title: "primary-container",
-    key: 1,
   },
   [
-    React.createElement("div", { key: 2 }, [
-      React.createElement("p", { key: 3 }, "React para text"),
-      React.createElement("img", {
-        key: 4,
-        src: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-        alt: 'react-image',
-        width: 220,
-        height: 220
-      }),
-    ]),
+    React.createElement(
+      "span",
+      {
+        id: "span-para",
+        key: "main-span-1",
+      },
+      "Sample para of React"
+    ),
+    React.createElement(
+      "span",
+      {
+        id: "span-para-1",
+        key: "main-span-2",
+      },
+      "Sample para of HTML"
+    ),
+    React.createElement(
+      "span",
+      {
+        id: "sample-para-2",
+        key: "main-span-3",
+      },
+      "Sample para of CSS"
+    ),
+    React.createElement("img", {
+      id: "sample-img",
+      key: "main-span-4",
+      src: "https://static.naukimg.com/s/0/0/i/ni-hamburger/nc_new_logo.svg",
+      alt: "image",
+    }),
   ]
 );
 
+const paragraphObj = {
+  $$typeof: Symbol.for("react.element"), // this property is must to render it in browser due to security reason..
+  type: "p", // this is also required
+  key: null, // optional but gives warning if not passed
+  ref: null, // optional
+  props: {
+    id: "para",
+    className: "container",
+    children: [
+      {
+        $$typeof: Symbol.for("react.element"), // this property is must to render it in browser due to security reason..
+        type: "span", // this is also required
+        key: null, // optional but gives warning if not passed
+        ref: null, // optional
+        props: {
+          id: "span-para",
+          children: "Paragraph sample of HTML",
+        }, // it's also required. it can be empty atleast if no prop passed.
+      },
+      {
+        $$typeof: Symbol.for("react.element"), // this property is must to render it in browser due to security reason..
+        type: "span", // this is also required
+        key: null, // optional but gives warning if not passed
+        ref: null, // optional
+        props: {
+          id: "span-para-1",
+          children: "Paragraph sample of CSS",
+        }, // it's also required. it can be empty atleast if no prop passed.
+      },
+      {
+        $$typeof: Symbol.for("react.element"), // this property is must to render it in browser due to security reason..
+        type: "span", // this is also required
+        key: null, // optional but gives warning if not passed
+        ref: null, // optional
+        props: {
+          id: "span-para-2",
+          children: "Paragraph sample of Javascript",
+        }, // it's also required. it can be empty atleast if no prop passed.
+      },
+    ],
+  }, // it's also required. it can be empty atleast if no prop passed.
+};
+
+console.log(paragraph);
+// console.log(paragraphObj);
 const root = ReactDOM.createRoot(document.querySelector("#main"));
-root.render(primaryContainer);
+
+root.render(paragraph);
