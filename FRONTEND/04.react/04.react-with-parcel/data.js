3a725df0e18bd38658c8cca66ff704d36744550d
Shubham Rawat <rawatshubam269@gmail.com>
2026-01-01 14:56:15 +0530

    updated : renovating/relarning parcel

--- FRONTEND/04.react/04.react-with-parcel/data.js
+++
@@ -1,3 +0,0 @@
-export const userData = {
-  user_name: "Shubham Rawat",
-};
--- FRONTEND/04.react/04.react-with-parcel/script.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-const heading = <h1>Hii Shubham This is Parcel you are learning and you are</h1>;
-
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(heading);
