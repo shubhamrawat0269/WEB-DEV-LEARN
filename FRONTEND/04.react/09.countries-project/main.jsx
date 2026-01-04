fbb80cfd5a6c8a8ee6ba355734654c8c8105afad
Shubham Rawat <rawatshubam269@gmail.com>
2026-01-04 12:57:42 +0530

    added : setup router

--- FRONTEND/04.react/09.countries-project/main.jsx
+++ FRONTEND/04.react/09.countries-project/main.jsx
@@ -1,5 +1,14 @@
-import ReactDOM from 'react-dom/client';
 import App from "./App";
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello World</div>,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
