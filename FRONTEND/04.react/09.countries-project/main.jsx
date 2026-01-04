7489adb46f19b40f531194c1bec75cab7e6e556f
Shubham Rawat <rawatshubam269@gmail.com>
2026-01-04 10:46:15 +0530

    updated : main file rename

--- FRONTEND/04.react/09.countries-project/main.jsx
+++ FRONTEND/04.react/09.countries-project/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from "./App";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+console.log(createBrowserRouter);
+console.log(RouterProvider);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
