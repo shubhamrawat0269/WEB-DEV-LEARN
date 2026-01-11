3d4f3e800427b754de5fff15e04606348b529f6e
Shubham Rawat <rawatshubam269@gmail.com>
2026-01-11 10:04:10 +0530

    updated : state persist

--- FRONTEND/04.react/09.countries-project/Layout/AppLayout.jsx
+++ FRONTEND/04.react/09.countries-project/Layout/AppLayout.jsx
@@ -3,7 +3,12 @@ import Header from "../components/Header"
 import { Outlet } from "react-router-dom"
 
 const AppLayout = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(JSON.parse(localStorage.getItem('isDark')));
+
+  useEffect(() => {
+    localStorage.setItem('isDark', isDark)
+  }, [isDark])
+
   return (
     <>
       <Header theme={[isDark, setIsDark]} />
