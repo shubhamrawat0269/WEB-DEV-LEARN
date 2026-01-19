8802bac7fe0387700d84a56098abd5b2f2992c36
Shubham Rawat <rawatshubam269@gmail.com>
2026-01-19 08:25:58 +0530

    updated : use vite for track expense project

--- FRONTEND/04.react/10.track-your-expense/components/ExpenseTrackerTotal.jsx
+++
@@ -1,9 +0,0 @@
-const ExpenseTrackerTotal = ({ totalExpense }) => {
-  return (
-    <div className="total-expense">
-      <strong>Total Expense:</strong> ₹{totalExpense}
-    </div>
-  )
-}
-
-export default ExpenseTrackerTotal
