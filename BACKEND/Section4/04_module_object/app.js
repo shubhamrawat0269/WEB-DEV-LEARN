e55b2b331c7ea797b15d26eb04f57c8593797683
Shubham Rawat <rawatshubam269@gmail.com>
2026-03-20 13:26:49 +0530

    Updated : Understand module wrapper fn

---
+++ BACKEND/Section4/04_module_object/app.js
@@ -0,0 +1,9 @@
+const sum = require('./math');
+const axios = require('axios');
+const num = 10;
+
+console.log(module)
+
+console.log(num);
+console.log(axios);
+console.log(sum(1,2));
---
+++ BACKEND/Section4/04_module_object/moduleWrapper.js
@@ -0,0 +1,3 @@
+(function (exports, require, module, __filename, __dirname) {
+  // Your code actually runs here
+});
