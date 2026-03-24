5b2b61f99835608fb9bd85212ab93f94b1cd4e7d
Shubham Rawat <rawatshubam269@gmail.com>
2026-03-24 15:46:27 +0530

    updated : understanding diff. b/w es6 and common js module system

--- BACKEND/Section4/06_es6_module/app.js
+++ BACKEND/Section4/06_es6_module/app.js
@@ -1,5 +1,10 @@
-import product, {num, sum} from './math.js';
+// import product, {num, sum} from './math.js';
+const { setupPrimary } = require('node:cluster');
+const {num, sum} = require('./math');
+const fs = require('node:fs');
+// console.log(this)
+console.log(fs)
 
-console.log(sum(150, 150));
-console.log(product(15, 15));
 console.log(num);
+console.log(sum(150, 150));
+// console.log(product(15, 15));
--- BACKEND/Section4/06_es6_module/math.js
+++ BACKEND/Section4/06_es6_module/math.js
@@ -1,6 +1,13 @@
-export let num = 550;
+// export let num = 550;
 
-export let sum = (a,b) => a+b;
-let product = (a,b) => a *b;
+// export let sum = (a,b) => a+b;
+// let product = (a,b) => a *b;
 
-export default product;
+// export default product;
+
+let num = 550;
+
+let sum = (a,b) => a+b;
+
+exports.num = num;
+exports.sum = sum;
