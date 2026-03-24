f56cf553e4b611f491b4162ca7c6a9270c9849b9
Shubham Rawat <rawatshubam269@gmail.com>
2026-03-24 14:47:53 +0530

    Added : understanding es6 module

---
+++ BACKEND/Section4/06_es6_module/app.js
@@ -0,0 +1,5 @@
+import product, {num, sum} from './math.js';
+
+console.log(sum(150, 150));
+console.log(product(15, 15));
+console.log(num);
---
+++ BACKEND/Section4/06_es6_module/math.js
@@ -0,0 +1,6 @@
+export let num = 550;
+
+export let sum = (a,b) => a+b;
+let product = (a,b) => a *b;
+
+export default product;
