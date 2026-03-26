6a2fee2323152cb0570392629fa938b1586d776f
Shubham Rawat <rawatshubam269@gmail.com>
2026-03-26 20:10:13 +0530

    Added : how to publish your own package and use it

---
+++ BACKEND/Section4/07_NPM_MODULES/app.js
@@ -0,0 +1,24 @@
+// import sumOfAP from './equations.js'
+import {sumOfnTerms,nthTerm, lastTerm, middleTerm} from 'equations'
+// const {sumOfnTerms,nthTerm, lastTerm, middleTerm} = require('equations')
+
+console.log(sumOfnTerms(10,7,5));
+console.log(nthTerm(2,6,3));
+console.log(lastTerm(2,10,5));
+console.log(middleTerm(4,500));
+
+/*
+A. Two types of npm : --  
+1.CLI tool  2. npm library to download packages.
+
+B. How to publish your package: 
+
+NOTE: first register and login into NPM website.
+
+1. Navigate to your package directory on command line terminal 
+using cd command.
+
+2. Use command : npm login
+3. npm publish
+
+*/
---
+++ BACKEND/Section4/08_npm_package/app.js
@@ -0,0 +1,6 @@
+import {sumOfnTerms,nthTerm, lastTerm, middleTerm} from 'shubham-arithmetic-formulas'
+
+console.log(sumOfnTerms(10,7,5));
+console.log(nthTerm(2,6,3));
+console.log(lastTerm(2,10,5));
+console.log(middleTerm(4,500));
