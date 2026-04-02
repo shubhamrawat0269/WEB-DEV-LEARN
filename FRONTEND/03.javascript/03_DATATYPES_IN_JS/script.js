9e6d72100dded5bd7cd2e32fff2de030492a2516
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-02 19:14:02 +0530

    Added : variables in js

---
+++ FRONTEND/03.javascript/03_DATATYPES_IN_JS/script.js
@@ -0,0 +1,23 @@
+/*
+--------------------------------
+
+A. JavaScript has 7 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+
+B. typeof keyword to check datatype in js.
+
+Bonus Point :  Backticks (``) also helpful to contain a set of characters which also has the power to hold javascript code . It can either be a vairable
+or a function returning some value. It support new line ('\n')
+
+C. NaN depicts invalid number but still it's a number . So, typeof gives you number 
+Bonue Point : praseInt() is slightly smart it tries to convert number if number pattern is like 99023Aavadslkjdlkfafsf;
+
+D. +undefined -> NaN , +null -> 0
+----------------------------------
+*/
---
+++ FRONTEND/03.javascript/04_VARIABLES_IN_JS/index.js
@@ -0,0 +1,7 @@
+let username = "Shubham Rawat";
+let age = 25;
+let profession = 'Web Developer';
+
+let userIntro = 'Hi I am ' + username + ' I am ' + age + ' year old';
+console.log(userIntro);
+
