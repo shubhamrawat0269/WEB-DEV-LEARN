222bb1404bde50c695009d21f61935946a132429
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-02 16:20:18 +0530

    Added : Understand history

---
+++ FRONTEND/03.javascript/01_HISTORY_OF_JS/app.js
@@ -0,0 +1,10 @@
+/*
+Notes : Brief Story 
+
+1. 90s web browser in demand - Netscape navigator
+2. Light and heavy calculations are dependent on Servers, making it less performant.
+3. Brendan Eich - Creator of Javascript earliery used to be known as Mocha -> LiveScript before Javscript
+
+Note : First Web Browser was created by Tim Berners-Lee and was called WorldWideWeb (later renamed Nexus)
+
+*/
