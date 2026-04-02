e750a8d6e3d8610fac2cdb27910c92e3620ba917
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-02 17:17:30 +0530

    Updated : JS intro, writing first program in js file and link it with html

---
+++ FRONTEND/03.javascript/02_INTRO_JS/app.js
@@ -0,0 +1,16 @@
+/*
+  REPL - Read Evaluate Print Loop.
+  JavScript is neither a compiled or interpreted language, but a mix of both
+  Browser request html from server i.e. live server in this case. 
+  HTML then request script or style sheet if found in html again from server i.e. liver server
+
+  Request are being shown on network tab.
+  Shortcut to clear the console tab code snippets using Ctrl + L or to use the clear() method but it leaves it's own impression also.
+  Also possible to link more than one script inside html .
+  BODMAS Rule not applicable on JS. JS includes operators precedency and () includes higher one. 
+*/
+
+let developerName = "Shubham Rawat";
+let profession = "Web Developer";
+
+console.log("Hii " + developerName + " Welcome to JS World. I am a" + " " + profession);
