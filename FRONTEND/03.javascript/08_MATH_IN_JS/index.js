b42cfd6eb2a5d8e6ed812774ce8aaad9f3d235e2
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-05 14:09:44 +0530

    Added : Math object in JS

--- FRONTEND/03.javascript/08_MATH_IN_JS/index.js
+++ FRONTEND/03.javascript/08_MATH_IN_JS/index.js
@@ -27,3 +27,14 @@ console.log(Math.floor(4.7)); // 4
 console.log(Math.max(1, 5, 3)); // 5
 console.log(Math.min(1, 5, 3)); // 1
 console.log(Math.random()); // Random number between 0 and 1
+
+console.log(Math.sqrt(25)); // 5
+console.log(Math.pow(2, 3)); // 8
+
+console.log(Math.abs(-5)); // 5
+console.log(Math.floor(Math.random() * 20 + 10)); // Random number between 1 and 10
+
+// Infinity keyword is a reserved in js which has a number type
+// It represents positive infinity & negative infinity.
+console.log(Infinity); // Infinity
+console.log(-Infinity); // -Infinity
