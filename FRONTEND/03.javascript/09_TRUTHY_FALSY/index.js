aa37ea0a5d2d4574c326ca8ad8a4261d4267bdad
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-05 19:45:47 +0530

    Added : Truthy/Falsy in jS

---
+++ FRONTEND/03.javascript/09_TRUTHY_FALSY/index.js
@@ -0,0 +1,23 @@
+/*
+  1. We can use the Boolean function to convert any value to a boolean. This is useful for checking if a value is truthy or falsy.
+  2. The following values are falsy in JavaScript:
+     - false
+     - 0
+     - ""
+     - null
+     - undefined
+     - NaN
+
+    3. All other values are truthy, some are:
+     - true
+     - 1
+     - "hello"
+     - {}
+     - []
+
+    Not Operation in JS - ! (exclamation mark) is used to negate a value. 
+    It converts a truthy value to false and a falsy value to true.
+
+The process of converting values to their boolean equivalent (truthy or falsy) in JavaScript is known as "type coercion" or "boolean coercion". This occurs when a value is evaluated in a context that expects a boolean, such as in an if statement or when using the logical NOT operator (!).
+
+*/
