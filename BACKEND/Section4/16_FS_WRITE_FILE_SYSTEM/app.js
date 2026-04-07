897fae7d8e07bbdd61ac62064388d2f99bf9f1a0
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-07 16:43:47 +0530

    updated : changes in writeFIleSync

--- BACKEND/Section4/16_FS_WRITE_FILE_SYSTEM/app.js
+++ BACKEND/Section4/16_FS_WRITE_FILE_SYSTEM/app.js
@@ -9,6 +9,15 @@
 
 import fs from 'node:fs/promises';
 
+const content = await fs.readFile('hello.txt');
+// console.log(content);
+fs.writeFile("C:/Users/Shubham Rawat/OneDrive/Desktop/file-2.txt", content);
+
+const contentImage = await fs.readFile("output.png");
+fs.writeFile(
+  "C:/Users/Shubham Rawat/OneDrive/Desktop/output.png",
+  contentImage,
+);
 
 // write file override the content of the file.
 // fs.writeFile('hello.txt', 'Hii I am shubham');
