b40b27f87b21d1b8083fea4f8c5e087b419065a5
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-04 09:14:50 +0530

    Added : Conceptual Learning in JS

---
+++ FRONTEND/03.javascript/01_INTERVIEW_PRACTICE/CONCEPT_BASED_LEARNING/index.js
@@ -0,0 +1,41 @@
+/*
+  Day 1 of learning Conceptual Based Learning
+  Topic : Var,let & Const
+  How Behind the Scene Javascript Execute code in two phases : 
+
+  Learning : 
+
+
+  // var let and const
+// scope
+// Variable shadowing
+
+var a = 5;
+let b = 10;
+
+function printname() {
+  let a = "Hii"; // variable shadowing....
+  var b = 20;
+  console.log(a);
+  console.log(b);
+}
+
+printname();
+
+// Declaration & Assignment 
+
+var num10 = 10;
+var num10 = 20;
+
+let num11 = 12;
+// let num11 = 10; // gives an error
+num11 = 'Shubham';  // can be reassign like var.
+
+*/
+
+
+/*
+  Day 2 of Learning JS 
+
+
+*/
