45f69b107c8cda5d6719d1d4e212eb6d44cdf6c3
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-15 09:35:48 +0530

    Added : Assignement Challenges for readableStreams

--- BACKEND/Section8/02_READABLE_STREAMS/index.js
+++ BACKEND/Section8/02_READABLE_STREAMS/index.js
@@ -1,8 +1,54 @@
-import fs from "fs/promises";
+/* 
+Assignment 1 — Read a Large File Chunk by Chunk 
+Understand how a readable stream sends data in chunks instead of loading the whole file into memory
 
-try {
-  const content = await fs.readFile("theme.mp4");
-  console.log(content.byteLength);
-} catch (error) {
-  console.error(error.message);
-}
+Task 
+1. Create a file named big.txt
+2. Add a lot of text into it (copy-paste paragraphs multiple times)
+3. Use fs.createReadStream() to read it.
+
+
+Solution : 
+
+------------------------------
+
+import fs from "fs";
+
+const readStream = fs.createReadStream("big.txt", {
+  highWaterMark: 1 * 1024 * 1024,
+});
+
+readStream.setEncoding("utf-8");
+
+readStream.on("data", (chunkBuffer) => {
+  fs.appendFileSync("new-big.txt", chunkBuffer);
+});
+
+------------------------------
+*/
+
+import fs from "fs";
+
+const readStream = fs.createReadStream("big.txt", {
+  highWaterMark: 1,
+});
+
+readStream.setEncoding("utf-8");
+
+let intialValue = 0;
+
+readStream.on("data", (chunkBuffer) => {
+  if (intialValue === 0) {
+    console.log("Copy Started....");
+    fs.writeFileSync("new-big.txt", chunkBuffer);
+  } else {
+    fs.appendFileSync("new-big.txt", chunkBuffer);
+  }
+
+  intialValue++;
+});
+
+readStream.on("end", () => {
+  console.log("Copying Ended.....");
+  console.log("Total Number of Words ", intialValue);
+});
