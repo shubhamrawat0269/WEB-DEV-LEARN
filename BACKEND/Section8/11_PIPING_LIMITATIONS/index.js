6a24a6017e5a507aae19e2d2fd87896d1cbfb555
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-25 07:44:29 +0530

    Updated : piping limitations

--- BACKEND/Section8/11_PIPING_LIMITATIONS/index.js
+++ BACKEND/Section8/11_PIPING_LIMITATIONS/index.js
@@ -1,6 +1,16 @@
 import fs from "fs";
+import { pipeline } from "stream";
 
 const readStream = fs.createReadStream("chars.txt", { highWaterMark: 4 });
 const writeStream = fs.createWriteStream("chars-new.txt", { highWaterMark: 4 });
 
-readStream.pipe(writeStream);
+pipeline(readStream, writeStream, (error) => {
+  console.log(error);
+});
+
+// readStream.pipe(writeStream);
+
+// setTimeout(() => {
+//   readStream.destroy("Readstream destroyed successfully");
+// }, 1);
+  
