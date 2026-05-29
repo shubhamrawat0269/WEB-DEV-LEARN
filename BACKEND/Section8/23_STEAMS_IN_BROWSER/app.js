a8c7a00c48f7ad663a87d51ea27c2aad0f60e82d
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-29 14:41:44 +0530

    Updated : learn about streams in browser

--- BACKEND/Section8/23_STEAMS_IN_BROWSER/app.js
+++ BACKEND/Section8/23_STEAMS_IN_BROWSER/app.js
@@ -1,10 +1,28 @@
 import http from "http";
+import fs from "fs/promises";
 
 const server = http.createServer(async (req, res) => {
   res.setHeader("access-control-allow-origin", "*");
-  res.setHeader("Content-Type", "video/mp4");
+  res.setHeader("Content-Type", "image/webp");
 
-  res.end("Welcome to my channel");
+  // res.end("Welcome to my channel Youtube Fellow");
+  const fileHandle = await fs.open("river.webp");
+
+  const readStream = fileHandle.createReadStream({ highWaterMark: 1 * 1024 });
+
+  readStream.on("data", (chunk) => {
+    res.write(chunk);
+    readStream.pause();
+
+    setTimeout(() => {
+      readStream.resume();
+    }, 500);
+  });
+
+  readStream.on("end", () => {
+    console.log("Data Send Successfully");
+    res.end();
+  });
 });
 
 server.listen(4000, "localhost", () => {
