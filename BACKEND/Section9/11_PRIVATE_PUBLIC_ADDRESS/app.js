b709a418f7ab95548ef99c398f74d06df59f270c
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-17 09:36:28 +0530

    Added : loop back explain

--- BACKEND/Section9/11_PRIVATE_PUBLIC_ADDRESS/app.js
+++ BACKEND/Section9/11_PRIVATE_PUBLIC_ADDRESS/app.js
@@ -1,9 +1,9 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.end("Hello, World!");
+  res.end("hello Shubham Rawat");
 });
 
-server.listen(4000, () => {
+server.listen(4000, "0.0.0.0", () => {
   console.log("Server is listening on localhost, port 4000");
 });
